ac291e187f8d4a6bf58bef677a9c2f3f47dc249a
zajk9999 <lukasz.zajkowski@gmail.com>
2024-10-14 15:40:38 +0100

    complete task 3

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const app = require("../app")
 const db = require("../db/connection")
 const seed = require("../db/seeds/seed")
 const data = require("../db/data/test-data")
+const endpoints = require("../endpoints.json")
 
 beforeEach(() => {
 	return seed(data);
@@ -13,7 +14,7 @@ afterAll(() => {
 });
 
 describe("/api/topics", () => {
-	it("200 - responds with array of topic objects", () => {
+	it("GET: 200 - responds with array of topic objects", () => {
 		return request(app)
 			.get("/api/topics")
 			.expect(200)
@@ -25,6 +26,17 @@ describe("/api/topics", () => {
 				});
 			});
 	});
-
 });
 
+describe("/api", () => {
+	it("GET: 200 - responds with an object with all available endpoints", () => {
+		return request(app)
+			.get("/api")
+			.expect(200)
+			.then(({ body }) => {
+				console.log(body)
+				expect(body.endpoints).toEqual(endpoints);
+			});
+	});
+
+});
