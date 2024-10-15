d09aec82214a9e528059e7b2e1d09f53d76d6665
zajk9999 <lukasz.zajkowski@gmail.com>
2024-10-15 11:07:21 +0100

    move error handlers to the separate file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const { getTopics, getArticleById, sendEndpoints } = require("./controllers/nc-news-controllers")
+const { psqlErrorHandler, customErrorHandler, serverErrorHandler } = require("./errorhandlers")
 const app = express()
 
 app.get("/api", sendEndpoints)
@@ -8,25 +9,12 @@ app.get("/api/topics", getTopics)
 
 app.get("/api/articles/:article_id", getArticleById)
 
+app.use(psqlErrorHandler)
 
-app.use((err, request, response, next) => {
-  if(err.code === '22P02') {
-    response.status(400).send({msg: 'Bad Request'})
-  }
-  next(err)
-})
+app.use(customErrorHandler)
 
-app.use((err, request, response, next) => {
-  if(err.status && err.msg) {
-    response.status(err.status).send({msg: err.msg})
-  }
-  next(err)
-})
+app.use(serverErrorHandler)
 
-
-app.use((err, request, response, next) => {
-  response.status(500).send({msg: "Internal Server Error"})
-})
 
 
 
