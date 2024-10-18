f9745790829c32799edcf60362498d8d51c8c48f
zajk9999 <lukasz.zajkowski@gmail.com>
2024-10-18 11:24:11 +0100

    add comment_count to get /api/articles/:article_id endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -54,6 +54,7 @@ describe("/api/articles/:article_id", () => {
 				expect(body.article.created_at).toBe("2020-11-03T09:12:00.000Z");
 				expect(body.article.votes).toBe(0);
 				expect(body.article.article_img_url).toBe("https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700");
+				expect(body.article.comment_count).toBe("2")
 			});
 	});
 	it("GET: 400 - responds with 'Bad Request' when given invalid ID as a parameter", () => {
--- models/nc-news-models.js
+++ models/nc-news-models.js
@@ -10,7 +10,7 @@ exports.fetchTopics = () => {
 
 exports.fetchArticleById = (article_id) => {
     return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [article_id]).then(({rows}) => {
+    .query(`SELECT articles.author, title, articles.article_id, topic, articles.body, articles.created_at, articles.votes, article_img_url, COUNT (comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`, [article_id]).then(({rows}) => {
         if(!rows.length) {
             return Promise.reject({status: 404, msg: 'Article does not exist'})
           }
